f35431567b0e552bdea2a51c9d52482e4c4c5eac
ajs <140010746+aboodJS@users.noreply.github.com>
2024-09-22 19:23:55 +0300

    added a button and some styling

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,10 @@ const formatedList = computed(() => {
 })
 
 async function fetchData() {
+  if (key.value === '' || key.value.length < 32) {
+    sect.value.innerHTML = 'enter a valid api key'
+    return 0
+  }
   const rq = await fetch(
     `${api.value}q=${formatedQuery.value}&from=${date.value || `${current.getFullYear()}-${String(current.getMonth() + 1).padStart(2, '0')}-${String(current.getDate()).padStart(2, '0')}`}&to=${date.value || `${current.getFullYear()}-${String(current.getMonth() + 1).padStart(2, '0')}-${String(current.getDate()).padStart(2, '0')}`}&apiKey=${key.value}`
   )
@@ -46,7 +50,7 @@ async function fetchData() {
     </span>
     <span>
       <label for="search">search</label>
-      <input type="search" name="search" id="" v-model="query" @keyup.exact.enter="fetchData" />
+      <input type="search" name="search" id="" v-model="query" />
     </span>
     <span>
       <label for="from">from</label>
@@ -56,12 +60,12 @@ async function fetchData() {
       <label for="to">to</label>
       <input type="date" name="to" id="" v-model="date2" @change="console.log(date2)" />
     </span>
+    <button @click="fetchData">search</button>
   </main>
   <h1>
     Note: due to the app using the free version of the API you are only limited to look up articles
     that are at most a month old
   </h1>
-  <hr />
   <section ref="sect">
     <div v-for="item in formatedList">
       <h1>{{ item.title }}</h1>
@@ -73,6 +77,12 @@ async function fetchData() {
 
 <style scoped>
 main {
+  border-radius: var(--br-radius);
+
+  padding: 15px;
+  justify-self: center;
+  width: fit-content;
+  background-color: var(--bg-color);
   text-align: center;
   justify-content: center;
   display: grid;
@@ -99,6 +109,10 @@ section {
 }
 
 div {
+  text-align: center;
+  border-radius: var(--br-radius);
+  background-color: var(--bg-color);
+  color: var(--line-color);
   font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
   font-size: 1rem;
 }
@@ -110,5 +124,10 @@ a {
 
 a:hover {
   text-decoration: underline;
+}
+
+h1 {
+  text-align: center;
+  border-bottom: 3px dotted var(--line-color);
 }
 </style>
